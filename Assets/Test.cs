80ec5acdb24156bd27533a70bec4de487c478c33
Behzad <Behzad.Bakhti@gmail.com>
2020-01-29 22:23:34 +0330

    first commit4

---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int Number;
+    
+    
+    private void OnEnable()
+    {
+        print("I am enabled");
+    }
+
+    public void TestFunction()
+    {
+        Debug.Log("I am testing");
+        
+    }
+// Start is called before the first frame update
+    void Start()
+    {
+        TestFunction();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
