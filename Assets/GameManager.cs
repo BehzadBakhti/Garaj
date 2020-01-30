9d222bb5ab117f6db2a56c7ccbc805ff885e9252
Morteza <morteza.jamshidi.link@gmail.com>
2020-01-30 14:45:27 +0330

    Sphere movement is done

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public SmoothOrbitCam camera = null;
+
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+
+    }
+}
---
+++ Assets/RunAwayObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAwayObject : MonoBehaviour
+{
+    public Vector3 velocity = new Vector3(0, 0);
+    public Vector3 targetVelocity = new Vector3(0, 0);
+    public float velocityAmount = 1;
+    private Vector3 lastMousePosition = new Vector3(0, 0, 0);
+    private float changeRate = 3;
+    private float lastChangeTime = 0;
+
+    private void Start()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        targetVelocity = velocity;
+        lastMousePosition = Input.mousePosition;
+        lastChangeTime = Time.time;
+    }
+
+    private void Update()
+    {
+        velocity = Vector2.Lerp(velocity, targetVelocity, Time.deltaTime);
+
+        transform.localPosition += velocity;
+        transform.localPosition += (Input.mousePosition - lastMousePosition)/20;
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, 0);
+        //lastMousePosition = Input.mousePosition;
+
+        CheckVelocity();
+    }
+
+    public void CheckVelocity()
+    {
+        if (Time.time - lastChangeTime >= changeRate)
+        {
+            lastChangeTime = Time.time;
+            targetVelocity = Random.onUnitSphere * velocityAmount;
+        }
+    }
+}
