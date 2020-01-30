f66de6ebd2fd433e575a56b79cb00202b33f45b3
Behzad <Behzad.Bakhti@gmail.com>
2020-01-30 14:39:38 +0330

    added do tween

---
+++ Assets/Scripts/Earth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+    [SerializeField] private ConcernPointSpawner _disasterSpawner, _resourceSpawner;
+
+    public void Spawn()
+    {
+        print("Spawn");
+        _disasterSpawner.Spawn();
+        _resourceSpawner.Spawn();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn();
+            print("Space");
+        }
+    }
+
+}
