bb76ba470ceb38f11538025b9fac94cd30521786
Behzad <Behzad.Bakhti@gmail.com>
2020-01-30 15:16:06 +0330

    corrected problem in the point

--- Assets/Scripts/ConcernPointSpawner.cs
+++ Assets/Scripts/ConcernPointSpawner.cs
@@ -14,27 +14,27 @@ public class ConcernPointSpawner : MonoBehaviour
     {
         RaycastHit hit = new RaycastHit();
         var center = transform.position;
-       Ray ray = new Ray();
-       // var direction = GenerateVector();
-        
+
+        var outPoint = GenerateEndPoint();
+        Ray ray = new Ray(outPoint,center-outPoint);
         var lr = gameObject.AddComponent<LineRenderer>();
         lr.SetPosition(0, center);
-       // lr.SetPosition(1, center+direction*5);
-        
+        lr.SetPosition(1, center + ray.direction * -5);
 
-        if (Physics.Raycast(ray,  out hit))
+
+        if (Physics.Raycast(ray, out hit))
         {
             if (hit.transform.GetComponent<Collider>() == _surface)
                 print(hit.point);
         }
     }
 
-    //private Ray GenerateRay(Vector3 origin, Vector3 destination)
-    //{
-    //    Ray ray= new Ray();
-    //    var outPos= new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f))*10;
+    private Vector3 GenerateEndPoint()
+    {
+     
+        return new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * 10;
 
-    //}
+    }
 
 
 
